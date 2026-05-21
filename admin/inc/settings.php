6f8683916410de26473eb3e94acda4d44cc2ad64
HuanTH <huan.huy.truong@gmail.com>
2026-05-21 11:46:44 +0700

    feat: replace settings link with donate link and add associated admin styles in plugin action links

--- admin/inc/settings.php
+++ admin/inc/settings.php
@@ -73,12 +73,39 @@ function image_points_callback(){
 
 function image_points_action_links( $links, $file ) {
     if ( strpos( $file, 'image-points.php' ) !== false ) {
-        $settings_link = '<a href="' . admin_url( 'edit.php?post_type=image_points&page=image-points' ) . '" title="'.__('Settings', 'image-points').'">' . __( 'Settings', 'image-points' ) . '</a>';
-        array_unshift( $links, $settings_link );
+        $donate_link = '<a class="image-points-donate-link" href="' . esc_url( 'https://paypal.me/nauhyuh99' ) . '" title="' . esc_attr__( 'Donate', 'image-points' ) . '" target="_blank" rel="noopener noreferrer">' . esc_html__( 'Donate', 'image-points' ) . '</a>';
+        array_unshift( $links, $donate_link );
     }
     return $links;
 }
 add_filter( 'plugin_action_links_' . IMAGE_POINTS_BASENAME, 'image_points_action_links', 10, 2 );
+
+function image_points_plugin_action_styles() {
+    ?>
+    <style>
+        .plugins a.image-points-donate-link {
+            align-items: center;
+            color: #b32d2e;
+            display: inline-flex;
+            font-weight: 600;
+            gap: 3px;
+        }
+
+        .plugins a.image-points-donate-link:hover,
+        .plugins a.image-points-donate-link:focus {
+            color: #8a2424;
+        }
+
+        .plugins a.image-points-donate-link .dashicons {
+            font-size: 16px;
+            height: 16px;
+            line-height: 1;
+            width: 16px;
+        }
+    </style>
+    <?php
+}
+add_action( 'admin_head-plugins.php', 'image_points_plugin_action_styles' );
 
 function image_points_get_options($name = ''){
     $options = wp_parse_args(get_option('image_points_options'),array(
