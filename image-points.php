e6dcb8083537320d9b9975bfb6eb5dd003d02464
HuanTH <huan.huy.truong@gmail.com>
2026-05-22 10:39:17 +0700

    chore: update plugin URI to repository URL

--- image-points.php
+++ image-points.php
@@ -7,7 +7,7 @@
 
 /*
 Plugin Name: Image Points
-Plugin URI: https://t11n.dev/
+Plugin URI: https://github.com/t11n-dev/image-points
 Description: Image Points helps you add interactive points to your images.
 Author: T11N
 Version: 1.0.0
